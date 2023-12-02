dbb94a5d67a00caaed8b2274e26171744ddcc280
Yazeed1777 <yazeed12347rf@gmail.com>
2023-12-02 23:23:13 +0300

    edit on dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,9 +8,8 @@
           <div class="row" style="display: inline-block;" >
           <div class="tile_count">
             <div class="col-md-2 col-sm-4  tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
+              <span class="count_top"><i class="fa fa-user"></i> Total patients</span>
               <div class="count">2500</div>
-              <span class="count_bottom"><i class="green">4% </i> From last Week</span>
             </div>
             <div class="col-md-2 col-sm-4  tile_stats_count">
               <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
