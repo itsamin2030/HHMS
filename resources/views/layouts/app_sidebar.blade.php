c427fbe624fb76d03858b610662bd14f96004e52
Amin <amoni1432@gmail.com>
2023-12-03 12:08:06 +0300

    edit sidebar

--- resources/views/layouts/app_sidebar.blade.php
+++ resources/views/layouts/app_sidebar.blade.php
@@ -52,24 +52,6 @@
                     </ul>
                   </li>
 
-
-                  <li><a><i class="fas fa-users"></i> @lang('admin_menu.hrm') <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="{{route('employee_role.index')}}">@lang('admin_menu.emp_roles')Employee Roles</a></li>
-                      <li><a href="{{route('employee.index')}}">@lang('admin_menu.emp_list')</a></li>
-                      <li><a href="{{route('employee.create')}}">@lang('admin_menu.add_emp')</a></li>
-
-                    </ul>
-                  </li>
-
-                  <li><a><i class="fas fa-dollar-sign"></i> @lang('admin_menu.finance') <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="/expense_category">@lang('admin_menu.exp_cat')</a></li>
-                      <li><a href="/expense/create">@lang('admin_menu.add_exp')</a></li>
-                      <li><a href="/expense">@lang('admin_menu.exp_list')</a></li>
-                    </ul>
-                  </li>
-
                   <li><a><i class="fas fa-pills"></i> @lang('admin_menu.medicine') <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="{{route('medicine_category.index')}}">@lang('admin_menu.medicine_cat')</a></li>
@@ -82,44 +64,6 @@
                     <ul class="nav child_menu">
                       <li><a href="{{route('prescription.create')}}">@lang('admin_menu.add_prescription')</a></li>
                       <li><a href="{{route('prescription.index')}}">@lang('admin_menu.prescription_list')</a></li>
-                    </ul>
-                  </li>
-
-                  <li><a><i class="fas fa-flask"></i> @lang('admin_menu.lab') <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="form.html">Add Department</a></li>
-                      <li><a href="form_advanced.html">Department List</a></li>
-                    </ul>
-                  </li>
-
-                  <li><a><i class="fa fa-hands"></i> @lang('admin_menu.donor') <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="{{route('donor.create')}}">@lang('admin_menu.add_donor')</a></li>
-                      <li><a href="{{url('donor')}}">@lang('admin_menu.donor_list')</a></li>
-                      <li><a href="{{url('bloodbank')}}">@lang('admin_menu.blood_bank')</a></li>
-                    </ul>
-                  </li>
-
-
-                  <li><a><i class="fas fa-bed"></i> @lang('admin_menu.bed_manager') <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                    <li><a href="{{ url('bed') }}">@lang('admin_menu.bed')</a></li>
-                      <li><a href="{{ url('bed_category') }}">@lang('admin_menu.bed_cat')</a></li>
-                      <li><a href="{{ url('floor') }}">@lang('admin_menu.bed_floor')</a></li>
-                    </ul>
-                  </li>
-
-
-                  <li><a><i class="fas fa-edit"></i> @lang('admin_menu.report') <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="{{url('appointmentReport')}}">@lang('admin_menu.app_report')</a></li>
-                    </ul>
-                  </li>
-
-                  <li><a><i class="fas fa-edit"></i> @lang('admin_menu.notice') <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="form.html">Add Department</a></li>
-                      <li><a href="form_advanced.html">Department List</a></li>
                     </ul>
                   </li>
 
